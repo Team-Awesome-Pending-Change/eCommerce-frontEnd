e50bb3562237e4939fc7ecf19979796327905a54
ReeceRenninger <renningerreece@gmail.com>
2023-07-21 09:56:53 -0700

    may delete Home.js as it only renders an h2 now, discuss with team before deleting files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,18 @@
 //! src/pages/Home.js
 import React from 'react';
-// import styled from 'styled-components';
-// import ProductCard from '../components/ProductCard';
+
 // import ProductCard from '../components/ProductCard';
 
 
-// const ProductGrid = styled.div`
-//   display: flex;
-//   flex-wrap: wrap;
-//   justify-content: center;
-// `;
+
+
 
 
 const Home = () => {
   return (
     <div>
       <h2>Welcome to our Store!</h2>
-      {/* <ProductCard /> */}
-      {/* <ProductGrid>
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </ProductGrid> */}
+
     </div>
   );
 };
