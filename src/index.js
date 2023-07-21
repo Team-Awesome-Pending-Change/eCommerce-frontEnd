2dd65c443121d8b82aac9b9483d3fa34498e9f7e
ReeceRenninger <renningerreece@gmail.com>
2023-07-21 12:19:44 -0700

    trying to resolve store.getState is not a function error after implementing redux store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import GlobalStyles from './styles/GlobalStyles';
-
+import store from './store';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
   <>
+  {/* had to invoke this here to fix an error? not sure if this is correct
+   */}
+  <Provider store={store}>
     <GlobalStyles />
     <App />
+  </Provider>
   </>
 );
 
