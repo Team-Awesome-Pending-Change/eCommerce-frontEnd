93bbbca4ecfafdb34985a6c645b47aaf7b1a7eb1
ReeceRenninger <renningerreece@gmail.com>
2023-07-21 13:56:21 -0700

    was mapping incorrectly at card.data.map only needed card.map for card component, successfully rendered the single card, need styling rework, image is gigantic and not all info is displaying correctly

--- src/store/cards.js
+++ src/store/cards.js
@@ -2,21 +2,24 @@ import axios from "axios";
 import { createSlice } from "@reduxjs/toolkit";
 
 export const getCards = () => async (dispatch) => {
-  let response = await axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Tornado%20Dragon');
-  dispatch(SET_CARDS(response.data.results));
-  console.log('this is the card data from the api using the getCards function: ', response.data.results);
+  try {
+    let response = await axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Tornado%20Dragon');
+    dispatch(setCards(response.data.data)); // Update the dispatch to use the correct action type and payload
+    console.log('this is the card data from the api using the getCards function: ', response.data.data);
+  } catch (error) {
+    console.error('Error fetching data from the API: ', error);
+  }
 };
 
 const cardSlice = createSlice({
   name: "cards",
   initialState: [],
   reducers: {
-    SET_CARDS: (state, action) => {
-      return {...state, cards: action.payload}
+    setCards: (state, action) => { // Update the reducer name to setCards
+      return action.payload; // Replace the state with the payload directly since it's an array
     }
   }
 });
 
-
-export const { SET_CARDS } = cardSlice.actions;
+export const { setCards } = cardSlice.actions; // Update the action export to use setCards
 export default cardSlice.reducer;
