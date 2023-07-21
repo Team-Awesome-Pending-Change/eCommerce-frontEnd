f93672610d447f5691cba6221a06807fb7bbc442
Ryan Eastman <rking1323@gmail.com>
2023-07-21 11:43:50 -0700

    updated home page with mui and a carousel to display images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,75 @@
 //! src/pages/Home.js
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import Carousel from "react-material-ui-carousel";
+import { Paper, Typography } from "@mui/material";
 
-// import ProductCard from '../components/ProductCard';
+const useStyles = makeStyles((theme) => ({
+  carouselImage: {
+    height: "400px",
+    width: "100%",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  carouselCaption: {
+    padding: theme.spacing(2),
+  },
+}));
 
-const Home = () => {
+const CarouselItem = ({ item }) => {
+  const classes = useStyles();
   return (
-    <div>
-      <h2>Welcome to our Store!</h2>
-
-    </div>
+    <Paper
+      className={classes.carouselImage}
+      style={{ backgroundImage: `url(${item.image})` }}
+    >
+      <Typography variant="h3" className={classes.carouselCaption}>
+        {item.caption}
+      </Typography>
+    </Paper>
   );
 };
 
+const Home = () => {
+  const classes = useStyles();
+  const items = [
+    {
+      image: "https://source.unsplash.com/featured/?yugioh",
+      caption: "Yu-Gi-Oh!",
+    },
+    {
+      image: "https://source.unsplash.com/featured/?magic",
+      caption: "Magic: The Gathering",
+    },
+    {
+      image: "https://source.unsplash.com/featured/?pokemon",
+      caption: "Pokemon",
+    },
+  ];
+
+  return (
+    <>
+      <Typography variant="h2" className={classes.header}>
+        Welcome to Mythical Card-Mart!
+      </Typography>
+      <Typography variant="body1" className={classes.description}>
+        Here we host a platform to buy your favorite trading cards. Have a collection that's missing a particular card? Check out our listings to see if you can find it! If you're looking to sell, soon we also have a platform for you to sell your cards to other collectors. 
+      </Typography>
+      <Carousel>
+        {items.map((item, i) => (
+          <CarouselItem key={i} item={item} />
+        ))}
+      </Carousel>
+    </>
+  );
+};
 
 export default Home;
+
+
+
+
